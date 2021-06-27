bd3b3a6c4ed6e07ee621867518677dd751017206
martin <martin_v_q@hotmail.com>
2021-06-27 12:43:45 -0500

    correccion inicio.js

--- public/assets/pages/scripts/inicio.js
+++ public/assets/pages/scripts/inicio.js
@@ -17,12 +17,8 @@ document.getElementById("foto").onchange = function(e) {
   reader.readAsDataURL(e.target.files[0]);
 }
 
-
-$(document).ready(function () {
-
-
-
-    $("#muro").on('submit', '.form-eliminar', function () {
+$(function () {
+    $("#muro").on('submit', '.form-eliminar', function (event) {
         event.preventDefault();
         const form = $(this);
         swal({
@@ -35,12 +31,12 @@ $(document).ready(function () {
             },
         }).then((value) => {
             if (value) {
-                ajaxRequest(form);
+                ajaxRequest2(form);
             }
         });
     });
 
-    function ajaxRequest(form) {
+    function ajaxRequest2(form) {
         $.ajax({
             url: form.attr('action'),
             type: 'POST',
@@ -116,6 +112,106 @@ $("#muro .me-gusta").on('click', function(e){
     $("#modalMegusta").modal("show");
     // console.log(id);
 });
-
-
 });
+
+// $(document).ready(function () {
+
+
+
+//     $("#muro").on('submit', '.form-eliminar', function (event) {
+//         event.preventDefault();
+//         const form = $(this);
+//         swal({
+//             title: '¿ Está seguro que desea eliminar el post ?',
+//             text: "Esta acción no se puede deshacer!",
+//             icon: 'warning',
+//             buttons: {
+//                 cancel: "Cancelar",
+//                 confirm: "Aceptar"
+//             },
+//         }).then((value) => {
+//             if (value) {
+//                 ajaxRequest(form);
+//             }
+//         });
+//     });
+
+//     function ajaxRequest(form) {
+//         $.ajax({
+//             url: form.attr('action'),
+//             type: 'POST',
+//             data: form.serialize(),
+//             success: function (respuesta) {
+//                 if (respuesta.mensaje == "ok") {
+//                     form.parents('.card').remove();
+//                     Biblioteca.notificaciones('El post fue eliminado correctamente', 'Ascensores Industriales', 'success');
+//                 } else {
+//                     Biblioteca.notificaciones('El registro no pudo ser eliminado, hay recursos usandolo', 'Ascensores Industriales', 'error');
+//                 }
+
+//             },
+//             error: function () {
+
+//             }
+//         });
+//     }
+
+
+// $("#muro").on("submit", ".form-like", function (e) {
+//     e.preventDefault();
+//     const form = $(this);
+//     let id= $(this).parent().find('.form-like').attr("data-id");
+//     let data = {
+//         id: id,
+//         _token: $("input[name=_token]").val(),
+//     };
+
+//     $.post("social/like/guardar/" + id, data, function (response) {
+//         form.find('button').addClass("text-primary");
+//         form.removeClass('form-like').addClass('form-dislike');
+//         let meGusta =form.parent().find(".me-gusta").html().split(' ');
+//         if (parseInt(meGusta[0])) {
+//             form.parent().find(".me-gusta").html((parseInt(meGusta[0])+1)+' me gusta');
+//         }else{
+//             form.parent().find(".me-gusta").html('1 me gusta');
+//         }
+//     });
+
+// });
+
+// $("#muro").on("submit", ".form-dislike", function (e) {
+//     e.preventDefault();
+//     const form = $(this);
+//     let id= $(this).parent().find('.form-dislike').attr("data-id");
+//     let data = {
+//         id: id,
+//         _token: $("input[name=_token]").val(),
+//     };
+
+//     $.post("social/like/eliminar/" + id, data, function (response) {
+//         form.find('button').removeClass("text-primary");
+//         form.removeClass('form-dislike').addClass('form-like');
+//         let meGusta =form.parent().find(".me-gusta").html().split(' ');
+//         if (parseInt(meGusta[0])==1) {
+//             form.parent().find(".me-gusta").empty();
+//         }else{
+//             form.parent().find(".me-gusta").html((parseInt(meGusta[0])-1)+' me gusta');
+//         }
+//     });
+
+// });
+
+
+// $("#muro .me-gusta").on('click', function(e){
+//     e.preventDefault();
+//     let id = $(this).attr("data-id");
+//     let data ={id};
+//     $.get("social/like/listar/"+id, data, function(response){
+//         $("#modalMegusta").find("div .modal-body").html(response);
+//     });
+//     $("#modalMegusta").modal("show");
+//     // console.log(id);
+// });
+
+
+// });
