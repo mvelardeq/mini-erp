258da5482dd1e5229c7e93ba6fefd9ca4b948c3b
martin <martin_v_q@hotmail.com>
2022-07-23 21:52:40 -0500

    change seeders estadoconceptopago

--- database/seeders/EstadoConceptoPagoTableSeeder.php
+++ database/seeders/EstadoConceptoPagoTableSeeder.php
@@ -17,16 +17,19 @@ class EstadoConceptoPagoTableSeeder extends Seeder
         \DB::table('estado_conceptopago')->delete();
 
         \DB::table('estado_conceptopago')->insert([
+            'id' => 1,
             'nombre' => 'Sin facturar',
             'created_at' => '2022-07-23 08:54:03',
             'updated_at' => '2022-07-23 08:54:03',
         ]);
         \DB::table('estado_conceptopago')->insert([
+            'id' => 2,
             'nombre' => 'Facturado',
             'created_at' => '2022-07-23 08:54:03',
             'updated_at' => '2022-07-23 08:54:03',
         ]);
         \DB::table('estado_conceptopago')->insert([
+            'id' => 3,
             'nombre' => 'Pagado',
             'created_at' => '2022-07-23 08:54:03',
             'updated_at' => '2022-07-23 08:54:03',
